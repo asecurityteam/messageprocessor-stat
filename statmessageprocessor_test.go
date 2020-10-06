39330afec43bf791bcffe4f5ffa83b6134133904
Aditya Oza <aoza@atlassian.com>
2020-10-06 09:37:01 -0500

    lower cc threshold

--- statmessageprocessor_test.go
+++ statmessageprocessor_test.go
@@ -13,6 +13,47 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
+func TestStatMessageProcessor_Component(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockMessageProcessor := NewMockMessageProcessor(ctrl)
+	mockStater := NewMockStat(ctrl)
+
+	statMessageProcessor := StatMessageProcessor{
+		ConsumedCounter:        consumedCounter,
+		ConsumerSuccessCounter: consumerSuccessCounter,
+		ConsumedSize:           consumedSize,
+		ConsumerLag:            consumerLag,
+		ConsumerTimingSuccess:  consumerTimingSuccess,
+		wrapped:                mockMessageProcessor,
+	}
+
+	incomingDataRecord := []byte(`{
+		"type":"CHANNEL",
+		"rawpayload":"{\"channel\":\"channelbob\",\"text\":\"the message\"}"
+	}`)
+	currentTime := time.Now()
+	sequenceNumber := "12345"
+	kinesisRecord := kinesis.Record{
+		Data:                        incomingDataRecord,
+		ApproximateArrivalTimestamp: &currentTime,
+		SequenceNumber:              &sequenceNumber,
+	}
+	gomock.InOrder(
+		mockStater.EXPECT().Timing(consumerLag, gomock.Any()),
+		mockStater.EXPECT().Count(consumedCounter, gomock.Any()),
+		mockStater.EXPECT().Count(consumedSize, gomock.Any()),
+		mockStater.EXPECT().Count(consumerSuccessCounter, gomock.Any()),
+		mockStater.EXPECT().Timing(consumerTimingSuccess, gomock.Any()),
+	)
+
+	mockMessageProcessor.EXPECT().ProcessMessage(gomock.Any(), gomock.Any()).Return(nil)
+	e := statMessageProcessor.ProcessMessage(xstats.NewContext(context.Background(), mockStater), &kinesisRecord)
+	assert.Nil(t, e)
+
+}
+
 func TestStatMessageProcessor_ProcessMessageSuccess(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
